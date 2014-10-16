854a86a956e1ef614f72f4e2c0309ff8cf55537f
scorpio374 <yvhynjw@gmail.com>
2014-10-16 18:43:11 +0800

    add location

--- src/cn/sz/free/gps/MainActivity.java
+++ src/cn/sz/free/gps/MainActivity.java
@@ -2,45 +2,126 @@ package cn.sz.free.gps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.MapView;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.LocationClientOption.LocationMode;
+//import com.baidu.mapapi.SDKInitializer;
+//import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends Activity {
 
-	MapView mMapView = null;
+//	private MapView mMapView = null;
+	public LocationClient mLocationClient;
+	public MyLocationListener mMyLocationListener;
+	private TextView resultText;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		//在使用SDK各组件之前初始化context信息，传入ApplicationContext  
         //注意该方法要再setContentView方法之前实现  
-        SDKInitializer.initialize(getApplicationContext());  
+//        SDKInitializer.initialize(getApplicationContext());  
         setContentView(R.layout.activity_main);  
+        resultText = (TextView)findViewById(R.id.tv_test);
         //获取地图控件引用  
-        mMapView = (MapView) findViewById(R.id.bmapView);  
+//        mMapView = (MapView) findViewById(R.id.bmapView); 
+        
+        //location
+        initLocationParams();
+        startLocation();
 	}
-	
-    @Override  
+
+	@Override  
     protected void onDestroy() {  
         super.onDestroy();  
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理  
-        mMapView.onDestroy();  
+//        mMapView.onDestroy();  
     }  
     
     @Override  
     protected void onResume() {  
         super.onResume();  
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理  
-        mMapView.onResume();  
+//        mMapView.onResume();  
     }  
     
     @Override  
     protected void onPause() {  
         super.onPause();  
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
-        mMapView.onPause();  
+//        mMapView.onPause();  
     } 
+    
+    private void initLocationParams() {
+		// TODO Auto-generated method stub
+        mLocationClient = new LocationClient(this);
+		mMyLocationListener = new MyLocationListener();
+		mLocationClient.registerLocationListener(mMyLocationListener);
+	}
+    
+    private void startLocation(){
+		LocationClientOption option = new LocationClientOption();
+		option.setLocationMode(LocationMode.Hight_Accuracy);//设置定位模式
+		option.setCoorType("gcj02");//返回的定位结果是百度经纬度，默认值gcj02
+		int span=1000;
+		try {
+			span = Integer.valueOf("1000");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		option.setScanSpan(span);//设置发起定位请求的间隔时间为5000ms
+		option.setIsNeedAddress(false);
+		mLocationClient.setLocOption(option);
+		mLocationClient.start();
+		Log.d("Debug", "location start");
+	}
+    
+	/**
+	 * 实现实位回调监听
+	 */
+	public class MyLocationListener implements BDLocationListener {
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			//Receive Location 
+			StringBuffer sb = new StringBuffer(256);
+			sb.append("time : ");
+			sb.append(location.getTime());
+			sb.append("\nerror code : ");
+			sb.append(location.getLocType());
+			sb.append("\nlatitude : ");
+			sb.append(location.getLatitude());
+			sb.append("\nlontitude : ");
+			sb.append(location.getLongitude());
+			sb.append("\nradius : ");
+			sb.append(location.getRadius());
+			if (location.getLocType() == BDLocation.TypeGpsLocation){
+				sb.append("\nspeed : ");
+				sb.append(location.getSpeed());
+				sb.append("\nsatellite : ");
+				sb.append(location.getSatelliteNumber());
+				sb.append("\ndirection : ");
+				sb.append("\naddr : ");
+				sb.append(location.getAddrStr());
+				sb.append(location.getDirection());
+			} else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+				sb.append("\naddr : ");
+				sb.append(location.getAddrStr());
+				//运营商信息
+				sb.append("\noperationers : ");
+				sb.append(location.getOperators());
+			}
+			Log.d("Debug", ""+sb.toString());
+			resultText.setText(sb.toString());
+		}
+	}
+	
 }
 
 
