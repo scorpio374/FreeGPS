96dc39bed9b281e46bda0fdb7138e251c7a24091
scorpio374 <yvhynjw@gmail.com>
2014-10-17 09:34:06 +0800

    Revert "add location"
    This reverts commit 97a5d5383b449ce3d4696ead6ac625a0bf511b73.

--- src/cn/sz/free/gps/MainActivity.java
+++ src/cn/sz/free/gps/MainActivity.java
@@ -9,25 +9,16 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapStatusUpdate;
-import com.baidu.mapapi.map.MapStatusUpdateFactory;
-import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MyLocationConfiguration;
-import com.baidu.mapapi.map.MyLocationData;
-import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
-import com.baidu.mapapi.model.LatLng;
+import com.baidu.location.LocationClientOption.LocationMode;
+//import com.baidu.mapapi.SDKInitializer;
+//import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends Activity {
 
-	private MapView mMapView = null;
+//	private MapView mMapView = null;
 	public LocationClient mLocationClient;
 	public MyLocationListener mMyLocationListener;
 	private TextView resultText;
-	BaiduMap mBaiduMap;
-	private LocationMode mCurrentMode;
-	boolean isFirstLoc = true;// 是否首次定位
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,51 +26,36 @@ public class MainActivity extends Activity {
 		
 		//在使用SDK各组件之前初始化context信息，传入ApplicationContext  
         //注意该方法要再setContentView方法之前实现  
-        SDKInitializer.initialize(getApplicationContext());  
+//        SDKInitializer.initialize(getApplicationContext());  
         setContentView(R.layout.activity_main);  
         resultText = (TextView)findViewById(R.id.tv_test);
         //获取地图控件引用  
-        mMapView = (MapView) findViewById(R.id.bmapView); 
-        mCurrentMode = LocationMode.NORMAL;
-        mBaiduMap = mMapView.getMap();
-		mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
-				mCurrentMode, true, null));
-		// 开启定位图层
-		mBaiduMap.setMyLocationEnabled(true);
-		// 定位初始化
-		mLocationClient = new LocationClient(this);
-		mMyLocationListener = new MyLocationListener();
-		mLocationClient.registerLocationListener(mMyLocationListener);
-		LocationClientOption option = new LocationClientOption();
-		option.setOpenGps(true);// 打开gps
-		option.setCoorType("bd09ll"); // 设置坐标类型
-		option.setScanSpan(1000);
-		mLocationClient.setLocOption(option);
-		mLocationClient.start();
+//        mMapView = (MapView) findViewById(R.id.bmapView); 
+        
         //location
-//        initLocationParams();
-//        startLocation();
+        initLocationParams();
+        startLocation();
 	}
 
 	@Override  
     protected void onDestroy() {  
         super.onDestroy();  
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理  
-        mMapView.onDestroy();  
+//        mMapView.onDestroy();  
     }  
     
     @Override  
     protected void onResume() {  
         super.onResume();  
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理  
-        mMapView.onResume();  
+//        mMapView.onResume();  
     }  
     
     @Override  
     protected void onPause() {  
         super.onPause();  
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
-        mMapView.onPause();  
+//        mMapView.onPause();  
     } 
     
     private void initLocationParams() {
@@ -91,6 +67,7 @@ public class MainActivity extends Activity {
     
     private void startLocation(){
 		LocationClientOption option = new LocationClientOption();
+		option.setLocationMode(LocationMode.Hight_Accuracy);//设置定位模式
 		option.setCoorType("gcj02");//返回的定位结果是百度经纬度，默认值gcj02
 		int span=1000;
 		try {
@@ -142,22 +119,6 @@ public class MainActivity extends Activity {
 			}
 			Log.d("Debug", ""+sb.toString());
 			resultText.setText(sb.toString());
-			
-			if (location == null || mMapView == null)
-				return;
-			MyLocationData locData = new MyLocationData.Builder()
-					.accuracy(location.getRadius())
-					// 此处设置开发者获取到的方向信息，顺时针0-360
-					.direction(100).latitude(location.getLatitude())
-					.longitude(location.getLongitude()).build();
-			mBaiduMap.setMyLocationData(locData);
-			if (isFirstLoc) {
-				isFirstLoc = false;
-				LatLng ll = new LatLng(location.getLatitude(),
-						location.getLongitude());
-				MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
-				mBaiduMap.animateMapStatus(u);
-			}
 		}
 	}
 	
